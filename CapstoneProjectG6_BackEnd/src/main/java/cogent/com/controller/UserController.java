8db540cb0c092d0b4ffaa27e84cabf09b7f36f62
prajeshsharma2000 <prajeshsharma2000@gmail.com>
2023-04-24 12:10:02 -0700

    home mapping

--- CapstoneProjectG6_BackEnd/src/main/java/cogent/com/controller/UserController.java
+++ CapstoneProjectG6_BackEnd/src/main/java/cogent/com/controller/UserController.java
@@ -1,5 +1,15 @@
 package cogent.com.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class UserController {
+    @GetMapping("/")
+    public String home() {
+        return "Home";
+    }
 
 }
