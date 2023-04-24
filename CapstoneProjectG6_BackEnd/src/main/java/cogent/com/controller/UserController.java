cdaed4ddf4e17163ff77361c3437d43ea50b81ee
prajeshsharma2000 <prajeshsharma2000@gmail.com>
2023-04-24 12:12:19 -0700

    adduser mapping

--- CapstoneProjectG6_BackEnd/src/main/java/cogent/com/controller/UserController.java
+++ CapstoneProjectG6_BackEnd/src/main/java/cogent/com/controller/UserController.java
@@ -1,15 +1,24 @@
 package cogent.com.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import cogent.com.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 public class UserController {
+    @Autowired
+    private UserService userService;
     @GetMapping("/")
     public String home() {
         return "Home";
+    }
+
+    @PostMapping("/adduser")
+    public ResponseEntity<?> addUser(@RequestBody UserDTO userDTO) {
+        User user = userService.addUser(userDTO);
+        return ResponseEntity.ok(user);
     }
 
 }
