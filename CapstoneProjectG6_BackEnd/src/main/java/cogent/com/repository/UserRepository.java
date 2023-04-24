8671a662cbd111d390e4d3d70cf53d98e2da746d
prajeshsharma2000 <prajeshsharma2000@gmail.com>
2023-04-24 12:41:37 -0700

    controllers

--- CapstoneProjectG6_BackEnd/src/main/java/cogent/com/repository/UserRepository.java
+++ CapstoneProjectG6_BackEnd/src/main/java/cogent/com/repository/UserRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import cogent.com.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
+    User findByUserName(String username);
 
+    User addUser(User user);
 }
