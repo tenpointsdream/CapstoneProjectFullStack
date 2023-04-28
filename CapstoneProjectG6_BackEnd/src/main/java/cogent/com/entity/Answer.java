3a6001b0249d92d72784fbd7553cdc5275bbba47
prajeshsharma2000 <prajeshsharma2000@gmail.com>
2023-04-27 19:23:47 -0700

    lombok in old backend

--- CapstoneProjectG6_BackEnd/src/main/java/cogent/com/entity/Answer.java
+++ CapstoneProjectG6_BackEnd/src/main/java/cogent/com/entity/Answer.java
@@ -1,7 +1,11 @@
 package cogent.com.entity;
 
+import lombok.*;
 import  javax.persistence.*;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "answer")
 public class Answer {
@@ -25,82 +29,4 @@ public class Answer {
 	@OneToOne
 	@JoinColumn(name = "created_by_id")
 	private User created_by;
-
-	public Answer() {}
-
-	public Answer(String description_answer, String img_src, String status, String datetime, Question question,
-			User approved_by, User created_by) {
-		this.description_answer = description_answer;
-		this.img_src = img_src;
-		this.status = status;
-		this.datetime = datetime;
-		this.question = question;
-		this.approved_by = approved_by;
-		this.created_by = created_by;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getDescription_answer() {
-		return description_answer;
-	}
-
-	public void setDescription_answer(String description_answer) {
-		this.description_answer = description_answer;
-	}
-
-	public String getImg_src() {
-		return img_src;
-	}
-
-	public void setImg_src(String img_src) {
-		this.img_src = img_src;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
-
-	public String getDatetime() {
-		return datetime;
-	}
-
-	public void setDatetime(String datetime) {
-		this.datetime = datetime;
-	}
-
-	public Question getQuestion() {
-		return question;
-	}
-
-	public void setQuestion(Question question) {
-		this.question = question;
-	}
-
-	public User getApproved_by() {
-		return approved_by;
-	}
-
-	public void setApproved_by(User approved_by) {
-		this.approved_by = approved_by;
-	}
-
-	public User getCreated_by() {
-		return created_by;
-	}
-
-	public void setCreated_by(User created_by) {
-		this.created_by = created_by;
-	}
-
 }
