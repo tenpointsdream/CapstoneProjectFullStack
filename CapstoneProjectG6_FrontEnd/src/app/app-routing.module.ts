4579e125f4785fc4dd69bccaebf61208e875c7ca
Abdu <abdu0921@hotmail.com>
2023-04-29 14:04:19 -0400

    router link works to navigate to user signup page

--- CapstoneProjectG6_FrontEnd/src/app/app-routing.module.ts
+++ CapstoneProjectG6_FrontEnd/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserLoginComponent } from './user-login/user-login.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'user-login', component: UserLoginComponent },
+
 ];
 
 @NgModule({
