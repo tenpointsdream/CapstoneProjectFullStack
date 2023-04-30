2c8a1c404fa31778fba245061e057a35bff0f331
prajeshsharma2000 <prajeshsharma2000@gmail.com>
2023-04-30 10:06:45 -0700

    pending answer

--- CapstoneProjectG6_FrontEnd/src/app/pending-answer/pending-answer.component.ts
+++ CapstoneProjectG6_FrontEnd/src/app/pending-answer/pending-answer.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Answer} from "../entity/answer.entity";
+import {QuestionService} from "../service/question.service";
+import {AnswerService} from "../service/answer.service";
+import {Question} from "../entity/question.entity";
 
 @Component({
   selector: 'app-pending-answer',
   templateUrl: './pending-answer.component.html',
   styleUrls: ['./pending-answer.component.css']
 })
-export class PendingAnswerComponent {
+export class PendingAnswerComponent implements OnInit {
+  answers:Answer[];
+
+  constructor(private answerService:AnswerService) {
+    this.answers = []
+  }
+  ngOnInit(): void {
+    this.answerService.getAnswers().subscribe((data:Answer[])=>{
+      console.log(data);
+      this.answers=data;
+    })
+  }
+
 
 }
