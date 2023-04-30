3ef88f08cc6ff1fde66f89dada604a5e756198f0
tenpointsdream <tenpointsdream@hotmail>
2023-04-30 15:13:07 -0400

    Fixed user login page

--- CapstoneProjectG6_FrontEnd/src/app/user-login/user-login.component.ts
+++ CapstoneProjectG6_FrontEnd/src/app/user-login/user-login.component.ts
@@ -6,8 +6,6 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { UserProfile } from '../entity/userprofile.entity';
-
-
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
@@ -17,6 +15,9 @@ export class UserLoginComponent {
   private baseUrl: string = "http://localhost:8080/user/login";
   userForm: User;
   model: User;
+  userChecked: User = new User();
+
+  role: string = '';
   constructor(
     private authenticationService: AuthenticationService,
     private httpClient: HttpClient,
@@ -28,29 +29,40 @@ export class UserLoginComponent {
   onSubmit(loginform: any) {
     this.userForm.username = loginform.value.username;
     this.userForm.password = loginform.value.password;
-    //alert(`Username: ${this.userForm.username} Password:${this.userForm.password}`);
-    this.httpClient.post('http://localhost:8080/user/authenticate',
-      { username: this.userForm.username, password: this.userForm.password },
-      { responseType: 'text' })
-      .subscribe((response: any) => {
-        const token = response;
-        this.cookieService.set('jwtToken', token, 1, '/');
-        console.log("Token: " + token);
-        this.httpClient.get<UserProfile>(`http://localhost:8080/user/getbyusername/${this.userForm.username}`,
-          {
-            headers: { Authorization: `Bearer ${token}` }
-            //withCredentials: true
-          })
-          .subscribe((userProfile: UserProfile) => {
-            console.log('Logged in as: ', userProfile.username);
-            this.cookieService.set('username', userProfile.username);
-            this.cookieService.set('name', userProfile.name);
-            console.log('Email: ', userProfile.email);
-            console.log('User type: ' + userProfile.userType);
-          });
-        this.router.navigate(['userhomepage']);
-      }, (err) => {
-        alert("Invalid username/password");
+    console.log("User name is: " + this.userForm.username);
+    console.log("User type before: " + this.role);
+    this.httpClient.get<UserProfile>(`http://localhost:8080/user/getbyusername/${this.userForm.username}`)
+      .subscribe((userdetails: UserProfile) => {
+        if (userdetails.userType.toString() === 'ADMIN') {
+          alert("FOR USERS ONLY!");
+        }
+        else {
+          //alert(`Username: ${this.userForm.username} Password:${this.userForm.password}`);
+          this.httpClient.post('http://localhost:8080/user/authenticate',
+            { username: this.userForm.username, password: this.userForm.password },
+            { responseType: 'text' })
+            .subscribe((response: any) => {
+              const token = response;
+              this.cookieService.set('jwtToken', token, 1, '/');
+              console.log("Token: " + token);
+              this.httpClient.get<UserProfile>(`http://localhost:8080/user/getbyusername/${this.userForm.username}`,
+                {
+                  headers: { Authorization: `Bearer ${token}` }
+                  //withCredentials: true
+                })
+                .subscribe((userProfile: UserProfile) => {
+                  console.log('Logged in as: ', userProfile.username);
+                  this.cookieService.set('username', userProfile.username);
+                  this.cookieService.set('name', userProfile.name);
+                  console.log('Email: ', userProfile.email);
+                  console.log('User type: ' + userProfile.userType);
+                });
+              this.router.navigate(['userhomepage']);
+            }, (err) => {
+              alert("Invalid username/password");
+            });
+        }
       });
+
   }
 }
