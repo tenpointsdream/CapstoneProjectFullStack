7b883e5a38bb3a3e471b66e5fe7027a4e9cc28d1
Abdu <abdu0921@hotmail.com>
2023-05-02 08:48:18 -0400

    minor change on controller

--- NewBackEnd/BackEnd/src/main/java/cogent/com/controller/UserController.java
+++ NewBackEnd/BackEnd/src/main/java/cogent/com/controller/UserController.java
@@ -1,30 +1,20 @@
 package cogent.com.controller;
 
-import java.util.List;
-import java.util.Optional;
-
+import cogent.com.entity.AuthRequest;
+import cogent.com.entity.User;
+import cogent.com.service.UserService;
+import cogent.com.util.JwtUtil;
+import cogent.com.util.UserType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import cogent.com.entity.AuthRequest;
-import cogent.com.entity.User;
-import cogent.com.service.UserService;
-import cogent.com.util.JwtUtil;
-import cogent.com.util.UserType;
-
-import static cogent.com.util.AppUtil.sha256;
+import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -52,10 +42,9 @@ public class UserController {
 //	}
 	@PostMapping("/authenticate")
 	public String generateToken(@RequestBody AuthRequest authRequest) throws Exception {
-		String encPassword = sha256(authRequest.getPassword());
 		try {
 			authenticationManager.authenticate(
-					new UsernamePasswordAuthenticationToken(authRequest.getUsername(), encPassword));
+					new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
 		} catch (Exception ex) {
 			throw new Exception("invalid username/password");
 		}
@@ -72,7 +61,6 @@ public class UserController {
 
 	@PostMapping("/adduser")
 	public ResponseEntity<User> addUser(@RequestBody User user) {
-		user.setPassword(sha256(user.getPassword()));
 		User newUser = userService.addNewUser(user);
 		return new ResponseEntity<>(newUser, HttpStatus.CREATED);
 	}
